fe5c3f47fc5e1fd5207f91a76f52597170c8a322
Ido <idoelishar81@gmail.com>
2024-01-10 16:40:08 +0200

    validation

--- src/inputs/teacher.inputs.ts
+++ src/inputs/teacher.inputs.ts
@@ -1,20 +1,37 @@
 /* eslint-disable prettier/prettier */
 
 import { Field, InputType } from "@nestjs/graphql";
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+
 
 @InputType()
 export class TeacherInput {
     @Field()
+    @IsString()
+    @IsNotEmpty()
     readonly name: string;
-    @Field()
-    readonly phone: string;
-    @Field()
-    readonly address: string;
-    @Field()
-    readonly email: string;
-    @Field()
-    readonly course: string;
-     @Field()
-    readonly images: string;
-  }
 
+    @Field()
+    @IsString()
+    @Length(10, 15) // Assuming phone numbers are between 10 and 15 characters
+    readonly phone: string;
+
+    @Field()
+    @IsString()
+    @IsNotEmpty()
+    readonly address: string;
+
+    @Field()
+    @IsEmail()
+    readonly email: string;
+
+    @Field()
+    @IsString()
+    @IsNotEmpty()
+    readonly course: string;
+
+    @Field()
+    @IsString()
+    @IsNotEmpty()
+    readonly images: string;
+}
